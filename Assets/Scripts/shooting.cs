3b0240363a0122b89bcb9f707c774389be55ea9a
Dayeon Choi <dayeon504@hanmail.net>
2020-08-03 04:19:36 +0900

    Unity Upload

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+   public GameObject bullet;
+   public GameObject parent;
+
+
+   void Start()
+   {
+       GameObject.Instantiate<GameObject>(bullet, parent.x,parent.y,parent.z,Quaternion.identity);
+   }
+   void Update(){
+       bullet.Transform(parent.x, parent.y*2f,parent.z);
+   }
+}
