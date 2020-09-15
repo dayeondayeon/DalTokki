e1ccf70d6ab2447c6851235b41c93abc8c26a150
Dayeon Choi <dayeon504@hanmail.net>
2020-09-16 05:43:57 +0900

    shooting 함수 수정

--- Assets/Scripts/BulletCreate.cs
+++ Assets/Scripts/BulletCreate.cs
@@ -13,32 +13,26 @@ public class BulletCreate : MonoBehaviour
     public GameObject player = null;
     void Start()
     {
-        in_pos = transform.position;
+        in_pos = transform.position; //첫 시작 위치(새로운 탄환이 생성될 위치)
+        respawnbullet();
     }
 
     void respawnbullet(){
             GameObject pinks  = GameObject.Instantiate(this.pink) as GameObject;
             pinks.transform.position = in_pos;
-            /*pos = in_pos;
-            pos.y += speed*Time.deltaTime;
-            pinks.transform.position = pos;
-            if(pos.y>13){
-                Destroy(pinks);
-            }*/
     }
-
     // Update is called once per frame
     void Update()
     {
         in_pos.x = player.transform.position.x;
-        in_pos.y = player.transform.position.y+0.5f;
+        in_pos.y = player.transform.position.y+0.1f;
         timecheck += Time.deltaTime;
-        if(timecheck>time){
-            timecheck = 0.0f;
+        if(timecheck>=time){
             this.respawnbullet();
-            Debug.Log("bullet create! " + time + " " + timecheck);
+            timecheck = 0.0f;
+            //Debug.Log("bullet create! " + time + " " + timecheck);
         }
-        Debug.Log("bullet end " + time + " " + timecheck);
+       // Debug.Log("bullet end " + time + " " + timecheck);
     }
 
     
--- Assets/Scripts/shooting.cs
+++ Assets/Scripts/shooting.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class shooting : MonoBehaviour
 {
   Vector2 pos;
-  float speed = 3;
+  float speed = 5;
   void Update(){
-    pos = transform.position;
+   /* pos = transform.position;
     pos.y += speed*Time.deltaTime;
     transform.position = pos;
     if(pos.y > 15)
     {
       Destroy(gameObject);
     }
-  }
+  }*/
 }
